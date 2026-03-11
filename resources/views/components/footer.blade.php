eb069acb00ce0b1710fbf6561eb03be4ddf64b79
KevinAzrazz <18223029@std.stei.itb.ac.id>
2026-03-11 15:28:31 +0700

    kecilin visit our article

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -53,7 +53,7 @@
         {{-- Visit Our Article Link --}}
         <div class="text-center mt-8 mb-12">
             <div class="gradient-bar rounded-full mx-auto mb-6"></div>
-            <a href="{{ route('articles.index') }}" class="text-3xl sm:text-4xl font-bold hover:opacity-80 transition" style="color: #00377D;">
+            <a href="{{ route('articles.index') }}" class="text-base sm:text-lg font-bold hover:opacity-80 transition" style="color: #00377D;">
                 Visit Our Article
             </a>
         </div>
